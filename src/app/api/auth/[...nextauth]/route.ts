c865df757dde5a4b185550db277530cec7c3db75
Amer Mekic <amerm3kic@gmail.com>
2025-06-09 01:23:23 +0200

    Preps

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,5 @@
-// src/app/api/auth/[...nextauth]/route.ts
-
-import { prisma } from "@/lib/db/prisma"
-import { env } from "@/lib/env"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { NextAuthOptions } from "next-auth"
+import authOptions from "@/lib/auth";
 import NextAuth from "next-auth"
-import { Adapter } from "next-auth/adapters"
-import GoogleProvider from "next-auth/providers/google"
-
-const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
-  providers: [
-    GoogleProvider({
-      clientId: env.GOOGLE_CLIENT_ID,
-      clientSecret: env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    session({ session, user }) {
-      session.user.id = user.id
-      return session
-    },
-  },
-}
 
 const handler = NextAuth(authOptions)
 export { handler as GET, handler as POST }
